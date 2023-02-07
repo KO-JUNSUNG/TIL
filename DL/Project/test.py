acfab0e5a357fe3f0dd941a91031a1b775c1a8ff
KO-JUNSUNG <rhwnstjd2004@gmail.com>
2023-02-07 19:25:05 +0900

    project update 2023 02 07 19:24 PM

--- DL/Project/test.py
+++ DL/Project/test.py
@@ -11,59 +11,12 @@ import zipfile
 import tempfile
 import os
 import tensorflow as tf
+import plotly
+import plotly.express as px
 
-# st.title("Image Processing")
-# st.write(
-#     "Upload an image below and I will annotate the faces in the image using [MediaPipe](https://mediapipe.dev/)."
-# )
-
-# file = st.file_uploader("Upload a file")
-# if file is None:
-#     file = st.camera_input("Or take a picture")
-
-# if file is not None:
-#     # Display original image
-#     st.write("You uploaded", file.name)
-#     col1, col2 = st.columns(2)
-#     col1.image(file)
-
-#     # Annotate image
-#     file_bytes = np.asarray(bytearray(file.read()), dtype=np.uint8)
-#     image = cv2.imdecode(file_bytes, 1)
-
-#     with mp.solutions.face_mesh.FaceMesh(static_image_mode=True) as face_mesh:
-#         results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-#         # Display annotate image
-#         if results.multi_face_landmarks:
-#             annotated_image = image.copy()
-
-#             for face_landmarks in results.multi_face_landmarks:
-#                 # st.code(face_landmarks)
-
-#                 mp.solutions.drawing_utils.draw_landmarks(
-#                     image=annotated_image,
-#                     landmark_list=face_landmarks,
-#                     connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,
-#                     landmark_drawing_spec=None,
-#                     connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_tesselation_style(),
-#                 )
-#                 mp.solutions.drawing_utils.draw_landmarks(
-#                     image=annotated_image,
-#                     landmark_list=face_landmarks,
-#                     connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-#                     landmark_drawing_spec=None,
-#                     connection_drawing_spec=mp.solutions.drawing_styles.get_default_face_mesh_contours_style(),
-#                 )
-
-#             col2.image(annotated_image, channels="BGR")
-#         else:
-#             col2.write("No faces found")
 
 # sample code 1 (model predicttion to csv)
 st.title('Pose reader')
-# Load pre-trained model
-model = load_model("Maded_model.h5")
 # Add a placeholder progress bar
 latest_iteration = st.empty()
 bar = st.progress(0)
@@ -77,7 +30,7 @@ for i in range(100):
 # load the model
 
 st.title("Load the Model")
-stream = st.file_uploader('Maded_model.h5.zip', type='zip')
+stream = st.file_uploader('TF.Keras model file (.h5py.zip)', type='zip')
 if stream is not None:
   myzipfile = zipfile.ZipFile(stream)
   with tempfile.TemporaryDirectory() as tmp_dir:
@@ -99,16 +52,32 @@ if file_path is not None:
     pred = model.predict(df2)    
     results = []
     for i in pred:
-        results.append(i.argmax())
-    output = np.mean(results)    
-    map_dict = {0: "middle", 1: "left", 2: "right"}
-    st.title(f"Your Pose is in the {output}. It means You're in the {map_dict[round(output)]} position.")
+        results.append(i.argmax()) # onehot decoding
+    output = round(np.mean(results),4)    
+    map_dict = {0: "left", 1: "middle", 2: "right"}
+    output_2 = output - 1
+    if output_2 > 0:
+        bias="right"
+    else:
+        bias="left"
+    st.title(f"Your Pose value is {output}. 0<=Value<=2, 0=left, 1=middle, 2=right. The value close to 1 is good.")
+    st.title(f"Your Pose is {map_dict[round(output)]}, but {round(output_2,4)} biased to {bias}.")
     
     df["Predictions"] = results
     pred_name = []
     for i in results:
         pred_name.append(map_dict[i])
     df["Predictions_name"] = pred_name
+    
+    label = df["Predictions_name"].value_counts()             ## bar 차트 그리기
+    st.dataframe( df.head() )
+    st.bar_chart(label)
+    #plotly pie차트
+    df["Counts"] = 1
+    df2 = df[["Predictions_name","Counts"]]
+    data = df.groupby("Predictions_name").sum()
+    fig1 = px.pie(data, values='Counts', names= data.index, title='자세 유형 분포도')
+    st.plotly_chart(fig1)
 
     # Save predictions to new CSV file
     save_path = st.text_input("If you want to save the file, Please write the file path down.:")
@@ -117,19 +86,3 @@ if file_path is not None:
         st.success("File saved successfully!")
 
             
-# sample code 2 (csv to barplot)
-
-# @st.cache
-# def load_data(file_path):
-#     return pd.read_csv(file_path)
-
-# file_path = st.file_uploader("Upload a CSV file", type="csv")
-# if file_path is not None:
-#     df = load_data(file_path)
-#     st.write("Columns:", list(df.columns))
-#     x = st.selectbox("Select a column for X-axis", df.columns)
-#     y = st.selectbox("Select a column for Y-axis", df.columns)
-#     fig = plt.figure()
-#     plt.bar(df[x], df[y])
-#     st.write(fig)
-    
